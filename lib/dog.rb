fb37107ce2d69117c8e4a04c5e7362f5a4f04d95
Brian DiRito <bdirito@gmail.com>
2018-09-20 12:24:03 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
   # code goes here
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood ||= "nervous"
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,16 @@
 class Fish
   # code goes here
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood ||= "nervous"
+  end
 end
