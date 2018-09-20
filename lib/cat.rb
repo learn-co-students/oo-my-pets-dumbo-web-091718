b6b71f227dc9a6eee336b19d9771679fbc738e2f
github username <github email address>
2018-09-20 11:51:49 -0400

    completed cat

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
+
+  attr_accessor :mood
+  attr_reader :name, :mood
+
   # code goes here
+  def initialize(name)
+   @name = name
+   @mood = 'nervous'
+  end
+          #
+          # def name
+          #   @name = "crookshanks"
+          # end
+
+          # def mood
+          #     @mood = mood
+          # end
 end
