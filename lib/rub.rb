dfd78084bf652f475b4ca76b92903dc5d8973809
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-09-20 13:30:15 -0400

    Done.

---
+++ lib/rub.rb
@@ -0,0 +1,13 @@
+# require './owner.rb'
+# require './fish.rb'
+# require './dog.rb'
+# require './cat.rb'
+#
+# require 'pry'
+#
+# jane = Owner.new("Jane")
+# jane.buy_dog("Daisy")
+# jane.buy_dog("Fido")
+# john = Owner.new("John")
+# binding.pry
+# puts "something"
